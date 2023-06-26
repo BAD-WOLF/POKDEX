c20876c53a6a5795bae7ec60326af9f75b191575
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-06-26 10:23:51 -0300

    refactor: change file create function with a fixed directory
    BREAKING CHANGE: The directory "$dir = getenv("HOME")" was commented to use a fixed
    directory, due to write permission issues on Windows

--- src/Controller/LikeUnlike.php
+++ src/Controller/LikeUnlike.php
@@ -14,35 +14,53 @@ class LikeUnlike extends AbstractController
     #[Route("/likeunlike/{status}", name: "LikeUnlike")]
     public function index($status)
     {
-        $dir = getenv("HOME");
-        system("touch " . $dir . "/like " . $dir . "/unlike");
+
+        #$dir = getenv("HOME"); // Retornou vazio e com status 500 (Permissão negada)
+        $dir = 'C:\Users\silas'; // Funciona /like e /unlike porque ta fixo o diretório
+
+        $likeFile = $dir . "/like";
+        $unlikeFile = $dir . "/unlike";
+
+        /*
+        var_dump($likeFile);
+        exit;
+        */
+
+        // Cria o arquivo "like" se não existir
+        if (!file_exists($likeFile)) {
+            touch($likeFile);
+        }
+
+        // Cria o arquivo "unlike" se não existir
+        if (!file_exists($unlikeFile)) {
+            touch($unlikeFile);
+        }
 
         if ($status == "like") {
-            if (filesize($dir . "/like") == 0) {
+            if (filesize($likeFile) == 0) {
                 $numL = 1;
-                file_put_contents($dir . "/like", $numL);
+                file_put_contents($likeFile, $numL);
             } else {
-                $dateL = intval(file_get_contents($dir . "/like"));
+                $dateL = intval(file_get_contents($likeFile));
                 $dateL++;
-                file_put_contents($dir . "/like", $dateL);
+                file_put_contents($likeFile, $dateL);
             }
         } elseif ($status == "unlike") {
-            if (filesize($dir . "/unlike") == 0) {
+            if (filesize($unlikeFile) == 0) {
                 $numU = 1;
-                file_put_contents($dir . "/unlike", $numU);
+                file_put_contents($unlikeFile, $numU);
             } else {
-                $dateU = intval(file_get_contents($dir . "/unlike"));
+                $dateU = intval(file_get_contents($unlikeFile));
                 $dateU++;
-                file_put_contents($dir . "/unlike", $dateU);
+                file_put_contents($unlikeFile, $dateU);
             }
         } else {
             throw new \InvalidArgumentException("Invalid status {{$status}}.");
         }
 
         return $this->json([
-            "like" => intval(file_get_contents($dir . "/like")),
-            "unlike" => intval(file_get_contents($dir . "/unlike"))
+            "like" => intval(file_get_contents($likeFile)),
+            "unlike" => intval(file_get_contents($unlikeFile))
         ]);
     }
 }
-
